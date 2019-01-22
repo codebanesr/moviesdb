51e5cfcef368bfbb74d13c7085334c754f919e41
shanurrahman <rshanur@deepiq.com>
2019-01-22 22:39:05 +0530

    Error
    You are not subscribed to this API.

--- moviedb-api/routes/index.js
+++ moviedb-api/routes/index.js
@@ -5,12 +5,12 @@ const XRapidAPIKey = 'f5108f6b7amsh47d0c6b24fd043fp112deejsn6c9a6fd2d106'
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  let r;
   unirest.get("https://devru-bigflix-movies-download-v1.p.rapidapi.com/movieDetails.php?movieid=16461")
     .header("X-RapidAPI-Key", XRapidAPIKey)
     .end(function (result) {
-      console.log(result.status, result.headers, result.body);
+    res.status(200).send(result);
     });
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
